542c4abcce841bef4ae334a72253adc13c830262
Shiva Sharma <shivathebravo@users.noreply.github.com>
2019-06-11 10:09:19 -0400

    Create registery.pp
    add registery code

---
+++ mainfests/registery.pp
@@ -0,0 +1,9 @@
+registery_key{'HKLM\Software\ShivaTest\PuppetOnWindows':
+    ensure=>'present',
+}
+
+registery_value{'HKLM\Software\ShivaTest\PuppetOnWindows':
+    ensure=>'present',
+    data=>'Registery Testing',
+    type=>'string',
+}
