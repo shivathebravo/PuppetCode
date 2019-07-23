250588a5cf09681d7a70f6da0f4e4c73789bd639
Shiva Sharma <shivathebravo@users.noreply.github.com>
2019-07-22 23:37:12 -0400

    Delete registery.pp

--- mainfests/registery.pp
+++
@@ -1,9 +0,0 @@
-registery_key{'HKLM\Software\ShivaTest\PuppetOnWindows':
-    ensure=>'present',
-}
-
-registery_value{'HKLM\Software\ShivaTest\PuppetOnWindows':
-    ensure=>'present',
-    data=>'Registery Testing',
-    type=>'string',
-}
